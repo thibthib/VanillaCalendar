d3608039eca089b075b656459f52262464afab1c
Thibaut Dutartre <t.dutartre@gmail.com>
2016-11-11 19:53:22 +0100

    Display week days

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 function getDateWithoutHours(date) {
 	var newDate = new Date(date);
-	newDate.setHours(0,0,0,0)
+	newDate.setHours(0,0,0,0);
 	return newDate;
 }
 
@@ -8,7 +8,7 @@ function isSameDay(day1, day2) {
 	return getDateWithoutHours(day1).getTime() === getDateWithoutHours(day2).getTime();
 }
 
-window.Calendar = function(element, dayToShow, events) {
+window.Calendar = function(selector, dayToShow, events) {
 	var firstDayOfWeek = 1; // Monday
 	var currentWeek = [];
 	
@@ -24,5 +24,51 @@ window.Calendar = function(element, dayToShow, events) {
 		});
 	}
 	
+	function getDayElement(day) {
+		function getDayLabel(day) {
+			if (window.Intl) {
+				return new Intl.DateTimeFormat(undefined, {
+					weekday: 'long',
+					month: 'long',
+					day: 'numeric'
+				}).format(day.date);
+			}
+			return day.date.toString().split(' ').reduce(function(label, bit, index) {
+				return index > 2 ? label : label + ' ' + bit;
+			}, '');
+		}
+		
+		var dayElement = document.createElement('div');
+		dayElement.classList.add('Calendar_day');
+		
+		var dayLabelElement = document.createElement('div');
+		dayLabelElement.classList.add('Calendar_dayLabel');
+		dayLabelElement.innerHTML = getDayLabel(day);
+		dayElement.appendChild(dayLabelElement);
+		
+		var dayContentElement = document.createElement('div');
+		dayContentElement.classList.add('Calendar_dayContent');
+		dayElement.appendChild(dayContentElement);
+		
+		return dayElement;
+	}
+	
+	function getWeekElement(week) {
+		var weekElement = document.createElement('div');
+		weekElement.classList.add('Calendar_week');
+		week.forEach(function(day) {
+			weekElement.appendChild(getDayElement(day));
+		});
+		
+		return weekElement;
+	}
+	
+	var calendarElement = document.querySelector(selector);
+	if (calendarElement) {
+		calendarElement.appendChild(getWeekElement(currentWeek));
+	} else {
+		throw 'Calendar : Provided selector didn\'t match any element';
+	}
+	
 	return currentWeek;
-}
+};
